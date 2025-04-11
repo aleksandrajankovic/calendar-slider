3585cdea85b40850e149bca7f26fa25039f7521b
aleksandrajankovic <aleksandra.bgd.87@gmail.com>
2025-04-11 13:40:10 +0200

    Inital commit

---
+++ assets/js/promotions.js
@@ -0,0 +1,110 @@
+const promotions = [
+  {
+    title: "PROMO 1",
+    subtitle:
+      "Promosheni hii itafanyika kwenye mchezo wa Aviator. <br>Wakati wa mashindano, wachezaji watakusanya pointi kama ifuatavyo: ",
+    image: "./assets/images/header/image1.jpg",
+    imageBox: "./img/logo-klubovi/slike/slider1.png",
+    description: [
+      "Kwa kila hisa 2,000 za TZS kwenye mchezo wa Aviator zikizingatiwa - unapata pointi 1. ",
+      "Kwa kila raundi inayochezwa kwenye mchezo wa Aviator - Unapata pointi 20.",
+      "Wachezaji 20 wa kwanza watatuzwa beti za bure za Aviator.",
+    ],
+
+    link: "https://promo.meridianbet.co.tz/sw/category/223/page/25440",
+    button: "Weka Pesa",
+  },
+  {
+    title: "OFA YA KILA SIKU JUMANNE ",
+    subtitle: "",
+    image: "./assets/images/header/image2.png",
+    imageBox: "./img/logo-klubovi/slike/slider1.png",
+    description: [
+      "Weka pesa ya kima cha chini kuanzia TZS 10,000 kwa kila kipindi cha bili, ili uweze kukidhi vigezo vya kushiriki katika promosheni ya Win & Go na urudishiwe 30% ya kile utakachopoteza. ",
+      "Wachezaji watarudishiwa Pesa kutokana na hasara iliyokokotolewa siku iliyotangulia, yaani, siku ambayo ofa ilifanya kazi.",
+    ],
+
+    link: "https://promo.meridianbet.co.tz/sw/category/223/page/25441",
+    button: "Weka Pesa",
+  },
+  {
+    title: "OFA YA KILA SIKU JUMATANO",
+    subtitle:
+      "Promosheni hii itafanyika kwenye mchezo wa Super Heli <br> Wakati wa mashindano, wachezaji watakusanya pointi kama ifuatavyo: ",
+    image: "./assets/images/header/image3.png",
+    imageBox: "./img/logo-klubovi/slike/slider1.png",
+    description: [
+      "Kwa kila hisa 5,000 za TZS kwenye mchezo wa Super Heli - unapata pointi 100.",
+      "Kwa kila raundi inayochezwa kwenye mchezo wa Super Heli - Unapata pointi 1.",
+      "Wachezaji 20 wa kwanza watatuzwa beti za bure za Super Heli.",
+    ],
+
+    link: "https://promo.meridianbet.co.tz/sw/category/223/page/25442",
+    button: "Weka Pesa",
+  },
+  {
+    title: "OFA ya Keno kila Alhamisi!",
+    subtitle:
+      "Weka angalau TZS 5,000 wakati wa siku ya promosheni na ucheze KENO ili ustahiki 20% ya kurudishiwa pesa kwa hasara yako yote ya siku ya promosheni!",
+    image: "./assets/images/header/image4.png",
+    imageBox: "./img/logo-klubovi/slike/slider1.png",
+    description: [],
+
+    link: "https://promo.meridianbet.co.tz/sw/category/223/page/25443",
+    button: "Weka Pesa",
+  },
+  {
+    title: "OFA YA KILA SIKU IJUMAA",
+    subtitle: "",
+    image: "./assets/images/header/image5.png",
+    imageBox: "./img/logo-klubovi/slike/slider1.png",
+    description: [
+      "Ofa ni halali kwa tiketi za kwanza zitakazochezwa kwa mpangilio ndani ya kipindi cha promosheni.",
+      "Malipo ya chini kwenye tiketi lazima yawe TZS 10,000",
+      "Kiwango cha chini cha Odd kwa kila tukio 1.30",
+      "Ikiwa tiketi itaonekana kuwa haijashinda, Meridian itakupa heshima kwa kurejesha malipo sawa na kiasi ulichoweka kama bonasi.",
+      "Ili mchezaji awe na haki ya kurejeshewa pesa, anahitaji kuweka pesa ndani ya kipindi cha promosheni",
+      "Kiwango cha chini cha malipo ya pesa kwa ajili ya kushiriki katika promosheni ni TZS 10,000",
+    ],
+
+    link: "https://promo.meridianbet.co.tz/sw/category/63/page/25444",
+    button: "Weka Pesa",
+  },
+  {
+    title: "OFA YA KILA SIKU JUMAMOSI",
+    subtitle: "",
+    image: "./assets/images/header/image6.png",
+    imageBox: "./img/logo-klubovi/slike/slider1.png",
+    description: [
+      "Dau ISHIRINI ZA KWANZA zenye thamani ya TZS 20,000 kwenye mchezo wa Kasino ya Wild Icy Fruits, zitapata spin 10 za bure kwenye mchezo huo huo endapo zikichezwa kwa dau la TZS 20,000, mchezo utarudisha spin 10 za bure na hivyo mara 20.",
+      "Spins zitalipwa automatiki, ambapo wachezaji wataarifiwa kwa ujumbe ndani ya mchezo wenyewe.",
+      "Spins ni halali ndani ya siku 3.",
+    ],
+
+    link: "https://promo.meridianbet.co.tz/sw/category/223/page/25445",
+    button: "Weka Pesa",
+  },
+  {
+    title: "OFA YA KILA SIKU JUMAPILI ",
+    subtitle:
+      "",
+    image: "./assets/images/header/image7.png",
+    imageBox: "./img/logo-klubovi/slike/slider1.png",
+    description: [
+      "Ili mchezaji aweze kuingia kwenye kinyang'anyiro cha kushinda zawadi, ni muhimu kulipa na kutumia pesa ya kiasi cha TZS 10,000 katika kipindi cha promosheni.",
+      "Matukio yote kwenye tiketi ambayo yatabashiriwa lazima yawe ni yale matukio yatakayohusisha mechi za ubashiri tu.",
+      "Tiketi zinazoshiriki katika shindano:",
+     
+    ],
+    description1:[
+        " • Malipo ya chini kwa kila tiketi ni 10,000 TZS",
+        " • Kiwango cha chini cha Odd kwa kila mechi ni 1.35",
+        " • Idadi ya chini ya mechi kwa kila tiketi ni 3",
+        " • Mshindi wa shindano lazima awe na idadi kubwa zaidi ya tiketi amabazo zimechezwa kwenye ubashiri wa michezo siku ya promosheni",
+  
+    ],
+    link: "https://promo.meridianbet.co.tz/sw/category/63/page/25446",
+    button: "Weka Pesa",
+  },
+];
+export default promotions;
